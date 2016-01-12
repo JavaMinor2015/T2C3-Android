97d5180f50825daed11f72fec46889ce981e22ad
Koen Vervoordeldonk <kmgvervo@avans.nl>
2016-01-12 14:46:38 +0100

    Retrofit basic implementation

--- app/src/main/java/com/infosupport/t2c3_android/activity/MainActivity.java
+++ app/src/main/java/com/infosupport/t2c3_android/activity/MainActivity.java
@@ -4,19 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.infosupport.t2c3_android.R;
 import com.infosupport.t2c3_android.adapter.OrderAdapter;
-import com.infosupport.t2c3_android.converter.JSONToOrder;
 import com.infosupport.t2c3_android.pojo.Order;
+import com.infosupport.t2c3_android.service.OrderService;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends Activity {
 
     private ListView orderList;
     private OrderAdapter orderAdapter;
+
+    Gson gson = new Gson();
+    public static final String BASE_URL = "http://localhost:6789";
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +49,27 @@ public class MainActivity extends Activity {
     }
 
     private void retrieveOrders() {
-        //TODO: implement REST call
 
-        String object = loadJSONFromAsset();
-        Order[] test = JSONToOrder.INSTANCE.convert(object);
-        for (Order order : test) {
-            orderAdapter.add(order);
-        }
+        OrderService orderService = retrofit.create(OrderService.class);
+        Call<List<Order>> callOrdersGetRequest = orderService.listOrders();
+        callOrdersGetRequest.enqueue(new Callback<List<Order>>() {
+
+            @Override
+            public void onResponse(Response<List<Order>> response) {
+                int statusCode = response.code();
+                List<Order> orders = response.body();
+
+                //TODO: foreach loop
+//                for (Order order : orders) {
+//                    orderAdapter.add(order);
+//                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                // Log error here since request failed
+            }
+        });
     }
 
     public String loadJSONFromAsset() {
---
+++ app/src/main/java/com/infosupport/t2c3_android/service/OrderService.java
@@ -0,0 +1,16 @@
+package com.infosupport.t2c3_android.service;
+
+import com.infosupport.t2c3_android.pojo.Order;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by koen on 1/12/16.
+ */
+public interface OrderService {
+    @GET("orders")
+    Call<List<Order>> listOrders();
+}
