c218a72cc39d52c6b6140a3f12087d3bf5fb4efa
Koen Vervoordeldonk <kmgvervo@avans.nl>
2016-01-12 23:15:20 +0100

    Added TODO'S for new activitiy's

---
+++ app/src/main/java/com/infosupport/t2c3_android/activity/OrderDetailFragment.java
@@ -0,0 +1,77 @@
+package com.infosupport.t2c3_android.activity;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.infosupport.t2c3_android.R;
+import com.infosupport.t2c3_android.activity.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Order detail screen.
+ * This fragment is either contained in a {@link OrderListActivity}
+ * in two-pane mode (on tablets) or a {@link OrderDetailActivity}
+ * on handsets.
+ */
+public class OrderDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+
+    //TODO: Change with PK column searched on ('id')
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    //TODO: Change with specific order POJO
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public OrderDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+
+            //TODO: Implement searching (REST CALL) mItem -> needs to be Order
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                //TODO: Set Order - ID as title
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.order_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        //TODO: Fill in TextView of specific order information
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.order_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
